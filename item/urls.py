635c6ea8ed5ebca776d9cfec0a44583e0ecc59dd
Girisha Devara <girisha.devara@gmail.com>
2020-04-14 19:25:31 +0530

    all CRUD operations completed

--- item/urls.py
+++ item/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("edit/<int:id>", views.edititem, name = "edititem"),
+    path("create",views.create, name = "createitem"),
+    path("delete/<int:id>",views.delete, name= "deleteitem"),
 ]
--- item/views.py
+++ item/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Item
 
 def index(request):
-    return render(request,'index.html')
+    items = Item.objects.all()
+    return render(request,'index.html',{"all_items":items})
+
+def edititem(request,id):
+    item = Item.objects.get(id = id)
+    if request.method == "POST":
+        name = request.POST.get('name')
+        cost = request.POST.get('cost')
+        image_url = request.POST.get('imgurl')
+        description = request.POST.get('description')
+        item.name = name
+        item.cost = cost
+        item.image_url = image_url
+        item.description = description
+        item.save()
+        return redirect('/item')
+    else:
+        return render(request,'edititem.html',{"item":item})
+
+def create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        cost = request.POST.get('cost')
+        image_url = request.POST.get('imgurl')
+        description = request.POST.get('description')
+        item = Item(name = name, cost = cost, image_url = image_url, description = description)
+        item.save()
+        return redirect('/item')
+    else:
+        return render(request,'createitem.html')
+
+def delete(request,id):
+    item = Item.objects.get(id = id)
+    item.delete()
+    return redirect('/item')
